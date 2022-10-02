0ec43d2112c50a6e8ca42c9896f7d675f272744c
Alexis Schad <a@mandelbro.com>
2022-10-02 23:30:59 +0200

    clients generation

--- world/table.gd
+++ world/table.gd
@@ -1,13 +1,24 @@
 extends Interactable
 class_name World_Table
 
+const receipes = ["espresso", "doppio", "cafe_au_lait"]
+
+var clients_scenes: Array[PackedScene] = [
+	preload("res://world/client1.tscn"),
+	preload("res://world/client2.tscn")
+]
+
 @onready var infoBulle: Node2D = $InfoBulle
+@onready var infoBulleLabel: Label = $InfoBulle/Label
 @onready var chairs: Array = $Chairs.get_children()
 
-var current_clients = null
+var current_clients: Array = []
 
 func _ready():
 	infoBulle.visible = false
+
+func is_empty():
+	return len(current_clients) == 0
 
 func on_interact_start():
 	infoBulle.visible = true
@@ -18,4 +29,12 @@ func on_interact_end():
 	print("quit table")
 
 func create_clients(nb_clients):
+	current_clients = []
+	for i in range(nb_clients):
+		current_clients.push_back(receipes[randi() % len(receipes)])
+		var client_scene = clients_scenes[randi() % len(clients_scenes)].instantiate()
+		chairs[i].add_child(client_scene)
 	
+	infoBulleLabel.text = ""
+	for client in current_clients:
+		infoBulleLabel.text += client + "\n"
