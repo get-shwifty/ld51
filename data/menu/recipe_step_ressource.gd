bff6fb1e6fee2a3b7e2b68ce86f86222631014a6
Damien Delmas <kerdelos@gmail.com>
2022-10-02 23:01:32 +0200

    changing data structures

---
+++ data/menu/recipe_step_ressource.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name RecipeStepRessource
+
+@export 
+var icon : PackedScene
+
+@export 
+var ingredients : Array[IngredientQuantityRessource]
+
+@export 
+var preparationTime : int
